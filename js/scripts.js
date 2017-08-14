b9aaeea0265e4a5b1404b68225059a24528c15e3
Elrey Bellmonti and Kelsey Langlois <elrey@>
2017-08-14 15:06:27 -0700

    Refactor js to use function to draw one card.

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,17 @@
+var suits = ["Spades", "Hearts", "Clubs", "Diamonds"];
+var values = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"];
+
 function getRandom(min, max) {
   return parseInt(Math.random() * (max - min) + min);
 }
 
+function drawOneCard() {
+  var suitIndex = getRandom(0, 4);
+  var valuesIndex = getRandom(0, 13);
+  return values[valuesIndex] + " of " + suits[suitIndex];
+}
+
 $(document).ready(function(){
-  var suits = ["Spades", "Hearts", "Clubs", "Diamonds"];
-  var values = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"];
 
   suits.forEach(function(suit){
     values.forEach(function(value){
@@ -15,13 +22,9 @@ $(document).ready(function(){
   $("#hand").click(function(){
     var pickedCards = []
     for(card = 0; card < 5; card+=1){
-      var suitIndex = getRandom(0, 4);
-      var valuesIndex = getRandom(0, 13);
-      var displayCard = values[valuesIndex] + " of " + suits[suitIndex];
+      var displayCard = drawOneCard();
       while (pickedCards.includes(displayCard)) {
-        var suitIndex = getRandom(0, 4);
-        var valuesIndex = getRandom(0, 13);
-        var displayCard = values[valuesIndex] + " of " + suits[suitIndex];
+        displayCard = drawOneCard();
       }
       pickedCards.push(displayCard);
       $(".drawn-hand").append("<li>" + displayCard + "</li>");
