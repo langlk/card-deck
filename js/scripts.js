5778b5e0a7317424d847126706e837be81741670
Elrey Bellmonti and Kelsey Langlois <elrey@>
2017-08-14 15:03:51 -0700

    Add button to draw a hand of five cards.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,7 @@
+function getRandom(min, max) {
+  return parseInt(Math.random() * (max - min) + min);
+}
+
 $(document).ready(function(){
   var suits = ["Spades", "Hearts", "Clubs", "Diamonds"];
   var values = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"];
@@ -6,5 +10,21 @@ $(document).ready(function(){
     values.forEach(function(value){
       $("#cards").append("<li>" + value + " of " + suit + "</li>");
     });
+  });
+
+  $("#hand").click(function(){
+    var pickedCards = []
+    for(card = 0; card < 5; card+=1){
+      var suitIndex = getRandom(0, 4);
+      var valuesIndex = getRandom(0, 13);
+      var displayCard = values[valuesIndex] + " of " + suits[suitIndex];
+      while (pickedCards.includes(displayCard)) {
+        var suitIndex = getRandom(0, 4);
+        var valuesIndex = getRandom(0, 13);
+        var displayCard = values[valuesIndex] + " of " + suits[suitIndex];
+      }
+      pickedCards.push(displayCard);
+      $(".drawn-hand").append("<li>" + displayCard + "</li>");
+    }
   });
 });
