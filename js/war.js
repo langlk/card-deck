257f3601b9269557b20be3ab981d41253eb7d7fe
Kelsey Langlois <kels.langlois@gmail.com>
2017-08-14 18:32:25 -0700

    Add comments and fix error in war case.

--- js/war.js
+++ js/war.js
@@ -2,17 +2,21 @@ var suits = ["Spades", "Hearts", "Clubs", "Diamonds"];
 var values = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"];
 var deck = [];
 
+// Gets a random integer i where min <= i < max
 function getRandom(min, max) {
   return parseInt(Math.random() * (max - min) + min);
 }
 
-function drawCard() {
+// "Draws" a random card from global deck, removing from array and returning.
+function drawCardFromDeck() {
   var cardIndex = getRandom(0, deck.length);
   var card = deck[cardIndex];
   deck.splice(cardIndex,1);
   return card;
 }
 
+// Takes two cards as "Value of Suit" strings
+// Returns string stating winner of round
 function warJudge (card1, card2) {
   var card1Array = card1.split(" ");
   var card2Array = card2.split(" ");
@@ -27,36 +31,42 @@ function warJudge (card1, card2) {
   }
 }
 
+// Draws a card from each player's hand and returns the cards as array
 function drawCards(playerOneHand, playerTwoHand) {
   var card1 = playerOneHand.pop();
   var card2 = playerTwoHand.pop();
   return [card1, card2];
 }
 
+// Prints the scores and results given to the page.
 function printHand(score1, score2, result) {
   $(".player1").prepend("<li>" + score1 + "</li>");
   $(".player2").prepend("<li>" + score2 + "</li>");
   $(".result").prepend("<li>" + result + "</li>");
 }
 
+// Handles a turn of War.
 function warTurn (playerOneHand, playerTwoHand) {
   var cards = drawCards(playerOneHand, playerTwoHand);
   var result = warJudge(cards[0], cards[1]);
   printHand(cards[0], cards[1], result);
-  while (result === "WAR!") {
+  while (result === "WAR!") { // Tie
+    // Each player draws 3 cards
     for (i = 1; i < 4; i += 1) {
+      debugger;
       cards = cards.concat(drawCards(playerOneHand, playerTwoHand));
       printHand(cards[cards.length - 2], cards[cards.length - 1], i);
     }
+    // Fourth card used to tiebreak, or start new War.
     cards = cards.concat(drawCards(playerOneHand,playerTwoHand));
-    var result = warJudge(cards[cards.length - 1], cards[cards.length - 2]);
-    printHand(cards[cards.length - 2], cards[cards.length - 1], i);
+    var result = warJudge(cards[cards.length - 2], cards[cards.length - 1]);
+    printHand(cards[cards.length - 2], cards[cards.length - 1], result);
   }
   if (result === "Player One Wins") {
     cards.forEach(function(card) {
       playerOneHand.unshift(card);
     });
-  } else {
+  } else { // Player Two Wins
     cards.forEach(function(card) {
       playerTwoHand.unshift(card);
     });
@@ -64,6 +74,7 @@ function warTurn (playerOneHand, playerTwoHand) {
 }
 
 $(document).ready(function(){
+  // Makes Deck array
   suits.forEach(function(suit){
     values.forEach(function(value){
       deck.push(value + " of " + suit);
@@ -73,13 +84,15 @@ $(document).ready(function(){
   var playerOneHand = [];
   var playerTwoHand = [];
 
+  // Makes Player Hands
   for (i = 0; i < 52; i+= 1) {
     if (i % 2 === 0){
-      playerOneHand.push(drawCard());
+      playerOneHand.push(drawCardFromDeck());
     } else {
-      playerTwoHand.push(drawCard());
+      playerTwoHand.push(drawCardFromDeck());
     }
   }
+
   $(".deck-one").text(playerOneHand.length);
   $(".deck-two").text(playerTwoHand.length);
 
